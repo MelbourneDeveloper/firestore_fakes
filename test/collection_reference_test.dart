599952248c97a691ac71433aeef4a676ab5f2632
Christian Findlay <16697547+MelbourneDeveloper@users.noreply.github.com>
2023-03-12 10:27:41 +1100

    Add a test

---
+++ test/collection_reference_test.dart
@@ -0,0 +1,48 @@
+import 'package:firestore_fakes/firestore_fakes.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  group('Tests for CollectionReferenceFake', () {
+    test('Test where', () async {
+      final expectedData = <String, dynamic>{
+        'name': 'John',
+        'age': 30,
+      };
+
+      final collectionRef = CollectionReferenceFake(
+        'my-collection',
+        getWhere: (
+          field, {
+          arrayContains,
+          arrayContainsAny,
+          isEqualTo,
+          isGreaterThan,
+          isGreaterThanOrEqualTo,
+          isLessThan,
+          isLessThanOrEqualTo,
+          isNotEqualTo,
+          isNull,
+          whereIn,
+          whereNotIn,
+        }) =>
+            field != 'name'
+                ? throw ArgumentError('Wrong field here')
+                : QueryFake(
+                    doGet: () async => QuerySnapshotFake(
+                      [QueryDocumentSnapshotFake(expectedData)],
+                    ),
+                  ),
+      );
+      final query = collectionRef.where('name', isEqualTo: 'John');
+
+      final querySnapshot = await query.get();
+
+      final firstQueryDocumentSnapshot = querySnapshot.docs.first;
+
+      final actualData = firstQueryDocumentSnapshot.data();
+
+      expect(actualData['name'], expectedData['name']);
+      expect(actualData['age'], expectedData['age']);
+    });
+  });
+}
